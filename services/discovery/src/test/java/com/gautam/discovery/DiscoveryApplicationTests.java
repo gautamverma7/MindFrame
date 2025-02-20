db81135bc633e88f05602291689a04569a18b1dc
gautamverma7 <gautamkumar.developer@gmail.com>
2025-02-20 23:18:33 +0530

    made apigateway service

---
+++ services/discovery/src/test/java/com/gautam/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gautam.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
